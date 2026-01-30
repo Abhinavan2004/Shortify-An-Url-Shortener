700ca674e9f49a253591ab10bf88cdc93f5e93b3
Abhinavan2004 <abhinavanpan@gmail.com>
2026-01-30 12:44:29 +0530

    Added repo and service files

---
+++ src/main/java/com/abhinav/URL_SHORTNER_APP/controller/Url_Controller.java
@@ -0,0 +1,8 @@
+package com.abhinav.URL_SHORTNER_APP.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Url_Controller {
+
+}
---
+++ src/main/java/com/abhinav/URL_SHORTNER_APP/service/Url_service.java
@@ -0,0 +1,8 @@
+package com.abhinav.URL_SHORTNER_APP.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Url_service {
+
+}
