ff3be9145f9718dc0b8e130385289f436c0d929e
Abhinavan2004 <abhinavanpan@gmail.com>
2026-01-30 12:26:42 +0530

    Added the initial project Files

---
+++ src/test/java/com/abhinav/URL_SHORTNER_APP/UrlShortnerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav.URL_SHORTNER_APP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
