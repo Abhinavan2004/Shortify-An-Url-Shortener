f44a4663a80be0732edfd3aa3f3db00380547010
Abhinavan2004 <abhinavanpan@gmail.com>
2026-01-31 11:58:58 +0530

    Updated Cors file

--- backend/src/main/java/com/abhinav/URL_SHORTNER_APP/controller/Url_Controller.java
+++ backend/src/main/java/com/abhinav/URL_SHORTNER_APP/controller/Url_Controller.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api")
 @RestController
 public class Url_Controller {
